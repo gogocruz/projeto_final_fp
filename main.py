bde199106360e21f94356aa13172a2efc494e097
DiogoCruz_22008318 <74059235+gogocruz@users.noreply.github.com>
2021-01-09 23:57:25 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -10,26 +10,43 @@ def main():
 
     screen = pygame.display.set_mode(res)
 
+    #musica
+    pygame.mixer.music.load('tlu2.ogg')
+    pygame.mixer.music.play(-1)
+
     #dar load à imagem
     fundo = pygame.image.load("fundo.png")
     zombie = pygame.image.load("zombie.png")
     human = pygame.image.load("human.png")
 
     while (True):
+        #processar dos eventos 
         events = pygame.event.get()
 
         for event in events:
             if (event.type == pygame.QUIT):
                 exit()
 
+        #Atualizar cena
+        k = pygame.key.get_pressed() 
+        if  k[pygame.K_ESCAPE]:
+            break # colocar para abrir menu
+ 
+        # Desenhar 
         screen.fill(screen_color)
 
-        #por p fundo no ecra
+        #por a imagens no ecra
         screen.blit(fundo, (300,100))
-        
+        screen.blit(zombie, (350,100))
+        screen.blit(zombie, (450,100))
+        screen.blit(zombie, (550,100))
+        screen.blit(zombie, (650,100))
+        screen.blit(human, (300,450))
 
-        pygame.draw.rect(screen, (200, 200, 200), (300, 100, 400, 400), 3)
+        #criacao do tabuleiro "fisico"
+        pygame.draw.rect(screen, (200, 200, 200), (300, 100, 400, 400), 3) #borda
 
+        #linhas verticais
         pygame.draw.line(screen, (200,200, 200), (300, 100), (300, 500), 3)
         pygame.draw.line(screen, (200,200, 200), (350, 100), (350, 500), 3)
         pygame.draw.line(screen, (200,200, 200), (400, 100), (400, 500), 3)
@@ -38,7 +55,7 @@ def main():
         pygame.draw.line(screen, (200,200, 200), (550, 100), (550, 500), 3)
         pygame.draw.line(screen, (200,200, 200), (600, 100), (600, 500), 3)
         pygame.draw.line(screen, (200,200, 200), (650, 100), (650, 500), 3)
-
+        #linhas horizontais
         pygame.draw.line(screen, (200,200, 200), (300, 150), (700, 150), 3)
         pygame.draw.line(screen, (200,200, 200), (300, 200), (700, 200), 3)
         pygame.draw.line(screen, (200,200, 200), (300, 250), (700, 250), 3)
@@ -48,12 +65,7 @@ def main():
         pygame.draw.line(screen, (200,200, 200), (300, 450), (700, 450), 3)
         pygame.draw.line(screen, (200,200, 200), (300, 500), (700, 500), 3)
 
-        #por as imagens no ecra
-        screen.blit(zombie, (350,100))
-        screen.blit(zombie, (450,100))
-        screen.blit(zombie, (550,100))
-        screen.blit(zombie, (650,100))
-        screen.blit(human, (300,450))
+     
 
 
         
