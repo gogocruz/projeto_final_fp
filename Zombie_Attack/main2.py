cca30505d0e3c7271726928f0c0debb4e1e80cc6
ritikjoshi24 <74405351+ritikjoshi24@users.noreply.github.com>
2021-01-13 20:50:08 +0000

    Player movement and turns added
    Player movement and turns added 
    Need to: add movement overlay and board limits and win conditions

--- Zombie_Attack/main2.py
+++ Zombie_Attack/main2.py
@@ -2,7 +2,7 @@ import pygame
 import pygame.freetype
 import setting2
 
-#Variables
+
 height = 700
 width = 1000
 dimension = 8
@@ -10,66 +10,22 @@ FPS=30
 square_size = height // dimension
 screen_color = (0, 0, 20)
 images ={}
-#END of Variables
 
-#NAME AND ICON OF THE GAME
-pygame.display.set_caption('Zombie Attack') 
-icon = pygame.image.load('characters/human.png') 
-pygame.display.set_icon(icon)
 
-#Functions 
 def loadImages():
         #dar load à imagem
     players=["human", "zombie"]
     for player in players:
-        images[player]= pygame.transform.scale(pygame.image.load("characters/"+ player + ".png"),(square_size,square_size))
+        images[player]= pygame.transform.scale(pygame.image.load("image/"+ player + ".png"),(square_size,square_size))
 
-def drawGameState(screen,gs):
-    drawBoard(screen)
-    drawPlayers(screen,gs.board)
-
-def drawBoard(screen):
-
-    colors= [pygame.Color("white"),pygame.Color("gray")]
-    for r in range(dimension):
-        for c in range(dimension):
-            color = colors[((r+c)% 2)]
-            pygame.draw.rect(screen, color , pygame.Rect( (c*square_size), (r*square_size),square_size+8,square_size+8))
-
-    fundo = pygame.transform.scale(pygame.image.load("images/fundo.png"),(700,700))
-    screen.blit(fundo, (0,0))
-
-    for j in range(dimension):
-        pygame.draw.line(screen, (200,200,200), ((square_size*j), 0), ((square_size*j), 700), 3)
-        pygame.draw.line(screen, (200,200,200), (0, (square_size*j)), (700,(square_size*j)), 3)
-            
-def drawPlayers(screen,board):
-    for r in range(dimension):
-        for c in range(dimension):
-            player = board[r][c]
-            if player != "--":
-                screen.blit(images[player],pygame.Rect( (c*square_size), (r*square_size),square_size+8,square_size+8))
-
-#END of Functions
-
-#Title Screen
 def title_screen():
-
     pygame.init()
     screen = pygame.display.set_mode((width, height))
     screen.fill(screen_color)
 
-    #musica
-    pygame.mixer.music.load('sounds/the_last_of_us2_music_theme.ogg')
-    pygame.mixer.music.play(-1)
-    pygame.mixer.music.set_volume(0.1)
-
-    #sons
-    bt = pygame.mixer.Sound("sounds/butons.mp3")
-
-    logo = pygame.image.load("images/logo.png")
-    play = pygame.image.load("images/play.png")
-    sair = pygame.image.load("images/exit.png")
+    logo = pygame.image.load("logo.png")
+    play = pygame.image.load("play.png")
+    sair = pygame.image.load("exit.png")
 
     while (True):
         events = pygame.event.get()
@@ -82,7 +38,9 @@ def title_screen():
                 if event.button == 1:
                     click = True
             
-    
+
+      
+
         #por p fundo no ecra
         screen.blit(logo, (0,0))
         screen.blit(play, (375, 400))
@@ -92,72 +50,96 @@ def title_screen():
 
         pos = pygame.mouse.get_pos()
 
+
         button_1 = pygame.Rect(375, 400, 235, 70)
         button_2 = pygame.Rect(375, 500, 232, 70)
 
         if button_1.collidepoint(pos):
             if (click == True):
-                bt.play()
                 main()
 
         if button_2.collidepoint(pos):
             if click:
-                bt.play()
                 exit()
-
 
         pygame.display.update()
         pygame.display.flip()
 
-
-#Game Screen
 def main():
-
     pygame.init()
     screen_color = (0, 0, 20)
     screen = pygame.display.set_mode((width,height))
     clock =pygame.time.Clock()
     screen.fill(screen_color)
     gs = setting2.GameState()
-   
+
+    validMoves = gs.getValidMoves()
+    moveMade = False
+
     loadImages()
     running= True
     sqSelected = () #to keep the track of last click of user(one tuple)
     playerClicks =[] # keep the track of player clicks(two tuple)
 
+
+
     while (running):
         #processar dos eventos 
         events = pygame.event.get() 
-        
         for event in events:
             if (event.type == pygame.QUIT):
                 exit()
-                
             # mouse button Functions
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 location = pygame.mouse.get_pos() # (x, y) location of mouse
-                print(location)
                 col = (location[0]//square_size)
                 row = (location[1]//square_size)
-
                 if sqSelected == (row , col): # user clicked the same square twice
                     sqSelected = () #deselect
                     playerClicks = [] #clear playerclick
-                    
                 else:
                     sqSelected = (row , col)
-                    playerClicks.append(sqSelected) # append for both 1st and 2nd clicks
-
+                    playerClicks.append(sqSelected) # append for both 1st and 2nd clicks        
                 if len(playerClicks) == 2: # after 2nd click
                     move = setting2.Move(playerClicks[0],playerClicks[1],gs.board)
-
-                    gs.makeMove(move)
+                    print(move.getChessNotation())
+                    if move in validMoves:
+                        gs.makeMove(move)
+                        moveMade = True
                     sqSelected = ()
                     playerClicks= []
+
+        if moveMade:
+            validMoves= gs.getValidMoves()
+            moveMade = False
 
         drawGameState(screen,gs)
         clock.tick(FPS)    
         pygame.display.flip()
 
+def drawGameState(screen,gs):
+    drawBoard(screen)
+    drawPlayers(screen,gs.board)
 
+def drawBoard(screen):
+
+    colors= [pygame.Color("white"),pygame.Color("gray")]
+    for r in range(dimension):
+        for c in range(dimension):
+            color = colors[((r+c)% 2)]
+            pygame.draw.rect(screen, color , pygame.Rect( (c*square_size), (r*square_size),square_size,square_size))
+
+    fundo = pygame.transform.scale(pygame.image.load("fundo.png"),(700,700))
+    screen.blit(fundo, (0,0))
+
+    for j in range(dimension):
+        pygame.draw.line(screen, (200,200, 200), ((square_size*j), 0), ((square_size*j), 700), 3)
+        pygame.draw.line(screen, (200,200, 200), (0, (square_size*j)), (700,(square_size*j)), 3)
+            
+def drawPlayers(screen,board):
+    for r in range(dimension):
+        for c in range(dimension):
+            player = board[r][c]
+            if player != "--":
+                screen.blit(images[player],pygame.Rect( (c*square_size), (r*square_size),square_size,square_size))
 title_screen()
--- Zombie_Attack/setting2.py
+++ Zombie_Attack/setting2.py
@@ -1,9 +1,7 @@
-import pygame
-
 class GameState():
     def __init__(self):
         self.board = [
-            ["--","zombie","--","zombie","--","zombie","--","zombie"],
+             ["--","zombie","--","zombie","--","zombie","--","zombie"],
             ["--","--","--","--","--","--","--","--"],
             ["--","--","--","--","--","--","--","--"],
             ["--","--","--","--","--","--","--","--"],
@@ -21,36 +19,86 @@ class GameState():
         self.moveLog.append(move)
         self.humanToMove = not self.humanToMove
 
+    def getValidMoves(self):
+        return self.getAllPossibleMoves()
+
+    def getAllPossibleMoves(self):
+        moves = []
+        for r in range(len(self.board)):
+            for c in range(len(self.board[r])):
+                turn = self.board[r][c][0]
+                if(turn =="h" and self.humanToMove) or (turn == "z" and not self.humanToMove):
+                    player = self.board[r][c][1]
+                    if player == "u":
+                        self.playerMoves(r, c, moves)
+                    elif player == "o":
+                        self.enemyMoves(r, c, moves)
+        return moves
+
+    def playerMoves (self, r, c, moves):
+        directions = ((-1, -1), (-1, 1), (1, -1), (1, 1)) #4 diaganols
+        enemy = "z" if self.humanToMove else "h"
+        for i in range(4):
+            endRow = r + directions[i][0]
+            endCol = c + directions[i][1]
+            if 0 <= endRow < 8 and 0 <= endCol < 8:
+                endPlayer = self.board[endRow][endCol]
+                if endPlayer == "--": # empty space valid
+                    moves.append(Move((r, c), (endRow, endCol), self.board))
+                elif endPlayer[0] == enemy: # enemy 
+                    break
+                else: # off board
+                    break
+
+    def enemyMoves (self, r, c, moves):
+        directions = ((1, -1), (1, 1)) #2 diaganols
+        player = "h" if not self.humanToMove else "z"
+        for j in range(2):
+            endRow = r + directions[j][0]
+            endCol = c + directions[j][1] 
+            if 0 <= endRow < 8 and 0 <= endCol < 8:
+                endPlayer = self.board[endRow][endCol]
+                if endPlayer == "--": # empty space valid
+                    moves.append(Move((r, c), (endRow, endCol), self.board))
+                elif endPlayer[0] == player: # enemy piece valid
+                    moves.append(Move((r, c), (endRow, endCol), self.board))
+                    break
+            else: # off board
+                break
+
+  #  
             
 
 class Move():
+    rankToRows = {"1":7, "2":6, "3":5, "4":4,
+                 "5":3, "6":2, "7":1, "8":0}
+    rowsToRanks = {v: k for k, v in rankToRows.items()}
+    filesToCols = {"a": 0, "b":1, "c": 2, "d": 3,
+                   "e": 4, "f":5, "g": 6, "h": 7}
+    colsToFiles = {v: k for k, v in filesToCols.items()}
 
     def __init__(self, startSq, endSq, board):
         self.startRow = startSq[0]
         self.startCol = startSq[1]
         self.endRow = endSq[0]
         self.endCol = endSq[1]
-        print(self.startRow,self.startCol)
         self.playerMoved = board[self.startRow][self.startCol] 
         self.playerCaptured = board[self.endRow][self.endCol]
         self.moveID = self.startRow* 1000 + self.startCol *100 +self.endRow*10 +self.endCol
 
+    
+                    
     '''
     Overriding the equal method
     '''
+    def __eq__(self, other):
+        if isinstance(other, Move):
+            return self.moveID == other.moveID
+        return False
 
-#    def __eq__(self, other):
-#        if isinstance(other, Move):
-#            return self.moveID == other.moveID
-#        return False
+    def getChessNotation(self):
+        return self.getRankFile(self.startRow,self.startCol) + self.getRankFile(self.endRow, self.endCol)
 
+    def getRankFile(self, r, c):
+        return self.colsToFiles[c] + self.rowsToRanks[r]
 
-def getValidMoves(self):
-    return self.getAllPossibleMoves()
-
-
-#def getAllPossibleMoves(self):
-#    moves = []
-#    for r in range (len(self.board)):
-#        for c in range(le(self.board[r])):
-            
