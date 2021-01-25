4031c0f833c322fd7a39926d139d42031c167645
DiogoCruz_22008318 <74059235+gogocruz@users.noreply.github.com>
2021-01-25 11:52:37 +0000

    Comments added
    Need to finish the comments and solve the zombie win condition and player clicks

--- Zombie_Attack/main.py
+++ Zombie_Attack/main.py
@@ -16,12 +16,15 @@ pygame.display.set_caption('Zombie Attack')
 icon = pygame.image.load('characters/human.png') 
 pygame.display.set_icon(icon)
 
-#Functions 
+# !!Functions! #
+
+# Load the images 
 def loadImages(): #dar load à imagem
     players=["human", "zombie"]
     for player in players:
         images[player]= pygame.transform.scale(pygame.image.load("characters/"+ player + ".png"),(square_size,square_size))
 
+#highlight the next valid positions
 def highlightSquares(screen, gs, validMoves, sqSelected):
 
     if sqSelected != ():
@@ -45,6 +48,7 @@ def drawGameState(screen,gs, validMoves, sqSelected):
     highlightSquares(screen, gs, validMoves, sqSelected)
     drawPlayers(screen,gs.board)
 
+#Draw all the game board 
 def drawBoard(screen):
     fundo = pygame.transform.scale(pygame.image.load("images/fundo.png"),(700,700))
     screen.blit(fundo, (0,0))
@@ -53,7 +57,8 @@ def drawBoard(screen):
     for j in range(dimension):
         pygame.draw.line(screen, (200,200, 200), ((square_size*j), 0), ((square_size*j), 700), 3)
         pygame.draw.line(screen, (200,200, 200), (0, (square_size*j)), (700,(square_size*j)), 3)
-            
+
+#Draw the images to the game board            
 def drawPlayers(screen,board):
     for r in range(dimension):
         for c in range(dimension):
@@ -65,25 +70,26 @@ def drawPlayers(screen,board):
 
 #Title Screen
 def title_screen():
-
+    # Initialize pygame, with the default parameters
     pygame.init()
     screen = pygame.display.set_mode((width, height))
     screen.fill(screen_color)
 
-    #musica
+    # Initialize the game music
     pygame.mixer.music.load('sounds/the_last_of_us2_music_theme.ogg')
     pygame.mixer.music.play(-1)
     pygame.mixer.music.set_volume(0.1)
 
-    #sons
+    #Initialize the butons sound and load them
     bt = pygame.mixer.Sound("sounds/butons.mp3")
     logo = pygame.image.load("images/logo.png")
     play = pygame.image.load("images/play.png")
     sair = pygame.image.load("images/exit.png")
 
+    # Game loop, runs forever
     while (True):
+        # Process OS events
         events = pygame.event.get()
-
         click = False
         for event in events:
             if (event.type == pygame.QUIT):
@@ -93,18 +99,17 @@ def title_screen():
                     click = True
             
 
-        #por p fundo no ecra
+        # Load and place the butons
         screen.blit(logo, (0,0))
         screen.blit(play, (375, 400))
         screen.blit(sair, (375, 500))
 
-        #comandos
-
-        pos = pygame.mouse.get_pos()
-
-
+        # Click bos of the exit and play butons
         button_1 = pygame.Rect(375, 400, 235, 70)
         button_2 = pygame.Rect(375, 500, 232, 70)
+
+        #Get mouse position
+        pos = pygame.mouse.get_pos()
 
         if button_1.collidepoint(pos):
             if (click == True):
@@ -116,48 +121,70 @@ def title_screen():
                 bt.play()
                 exit()
 
+        #keeps the display updated
         pygame.display.update()
+        # Swaps the back and front buffer, effectively displaying what we rendered
         pygame.display.flip()
 
 
 #Game Screen
 def main():
-    pygame.init()
-    screen_color = (0, 0, 20)
-    screen = pygame.display.set_mode((width,height))
-    clock =pygame.time.Clock()
-    screen.fill(screen_color)
-    gs = setting.GameState()
 
+    # Initialize pygame, with the default parameters
+    pygame.init()
+
+    # Define the size/resolution of our window with the values of widht and height
+    screen = pygame.display.set_mode((width,height))
+    # Clears the screen with a very dark blue (0, 0, 20)
+    screen_color = (0, 0, 20)
+    screen.fill(screen_color)
+    #Create a clock to track an amount of time
+    clock =pygame.time.Clock()
+
+    #Seting the Gamestate class to the variable gs
+    gs = setting.GameState()
+    
+    #Seting the getValidMoves function to the variable validMoves
     validMoves = gs.getValidMoves()
+    # Set move made and game over to default False value
     moveMade = False
     GameOver = False
+    #Load the images to the screen
     loadImages()
+    #Variable that keeps the Game loop to runs forever
     running= True
+
     sqSelected = () #to keep the track of last click of user(one tuple)
     playerClicks =[] # keep the track of player clicks(two tuple)
+
+    #Loaf the buton of exit and it  sound
     sair = pygame.image.load("images/exit.png")
     bt = pygame.mixer.Sound("sounds/butons.mp3")
-  
+    
     while (running):
-        #processar dos eventos 
+
+        #Setting the pygame.event to the variable events
         events = pygame.event.get() 
+        #Setting the mouse position 
         pos = pygame.mouse.get_pos()
+        #Setting the colision bos to this coordinates with the variable button_3
         button_3 = pygame.Rect(740,600, 235, 70)
-        
+
+        # Process OS events 
         click = False
         for event in events:
             if (event.type == pygame.QUIT):
+                # Checks if the user closed the window
                 exit()
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                
+                    # Checks the user mouse clicks with his location in x and y
                     location = pygame.mouse.get_pos() # (x, y) location of mouse
                     col = (location[0]//square_size)
                     row = (location[1]//square_size)
-
+                    #Check if the player cick woth the left buton of the mouse
                     if event.button == 1:
                         click = True
-
+                    #Until is not game over
                     if not GameOver:
                         try:
                             if sqSelected == (row , col): # user clicked the same square twice
@@ -167,35 +194,44 @@ def main():
                             else:
                                 sqSelected = (row , col)
                                 playerClicks.append(sqSelected) # append for both 1st and 2nd clicks 
-
+                            #Put the clisk in a list with the value of 2
                             if len(playerClicks) == 2: # after 2nd click
+                                #Turning the two clicks into a move
                                 move = setting.Move(playerClicks[0],playerClicks[1], gs.board)
+                                #Checking if two clicks are a move
                                 for i in range(len(validMoves)):
                                     if move == validMoves[i]:
                                         gs.makeMove(validMoves[i])
                                         moveMade = True
                                         sqSelected = ()
                                         playerClicks = []
+                                #If not is not a valid move clears the list of clicks       
                                 if not moveMade:
+                                    sqSelected = ()
                                     playerClicks = []
+                        #If nothing above as been read, pass            
                         except:
                             pass
-                        
+        #If the clicks are inside the game board and dont make valid move, then no move motivated                
         if moveMade:
             validMoves= gs.getValidMoves()
             moveMade = False
-
+        #Check ig the exit button  is clicked
         if button_3.collidepoint(pos):
             if (click == True):
                 bt.play()
                 title_screen()
-            
+        #LOad to the screen the exit buton     
         screen.blit(sair, (740,600))
+        #Check if is any win condition
         if gs.humanWin or gs.zombieWin:
             GameOver = True
             
         drawGameState(screen,gs, validMoves, sqSelected)
-        clock.tick(FPS)    
+        #Set the amount of time to the variable FPS wich is 60
+        clock.tick(FPS) 
+        # Swaps the back and front buffer, effectively displaying what we rendered   
         pygame.display.flip()
 
+#Calling the title screen to something apear :D
 title_screen()
--- Zombie_Attack/setting.py
+++ Zombie_Attack/setting.py
@@ -1,10 +1,13 @@
 import pygame 
-
+#Load the images of Win
 humanwin = pygame.image.load("images/humanwins.png")
 zombiewin = pygame.image.load("images/zombieswin.png")
 
+#Class wich is checking all the game state
 class GameState():
+
     def __init__(self):
+        #Setting the  gameboard into a matris with the starting location of the characters 
         self.board = [
             ["--","zombie","--","zombie","--","zombie","--","zombie"],
             ["--","--","--","--","--","--","--","--"],
@@ -15,40 +18,65 @@ class GameState():
             ["--","--","--","--","--","--","--","--"],
             ["--","--","--","--","human","--","--","--"]
         ]
+        #Seeting the first turn ro human(sheep)
         self.humanToMove = True
+        #Turn the win conditions to false
         self.humanWin = False
         self.zombieWin = False
-        
+
+    #Make movement    
     def makeMove(self, move):
+        #Check if the next square is empty 
         self.board[move.startRow][move.startCol] = "--"
+        #If yes load the new movemnt
         self.board[move.endRow][move.endCol] = move.playerMoved
+        #
         self.humanToMove = not self.humanToMove
+        #IF the human reach the first line its human win
         if move.gameover:
-            self.humanWin = True
+            #Turning the human win true
+            self.humanWin = True         
+            #Turning again the window dimensions to 1000,700   
             screen = pygame.display.set_mode((1000, 700))
+            # Clears the screen with a very dark blue (0, 0, 20)
             screen_color = (0, 0, 20)
             screen.fill(screen_color)
+            #Load the image of human win in given coordinates
             screen.blit(humanwin, (750,40))
+        #If dont, just continue the game loop    
         else :
             pass
                     
     def getValidMoves(self):
+        #Start the list of moves empty
         moves = []
+        #Setting to the variable Moves all the possivle moves function 
         moves = self.getAllPossibleMoves()
+        #check if the human has no more possible moves
         if not moves and self.humanToMove:
+            #Turning the zombie win to true
             self.zombieWin = True
+            #Turning again the window dimensions to 1000,700 
             screen = pygame.display.set_mode((1000, 700))
+            # Clears the screen with a very dark blue (0, 0, 20)
             screen_color = (0, 0, 20)
             screen.fill(screen_color)
+            #Load the image of zombie win in given coordinates
             screen.blit(zombiewin, (750,40))
+        #return the vali moves    
         return moves
 
     def getAllPossibleMoves(self):
+        #Start the list of moves empty
         moves = []
+        #Check if the move is inside the board rows
         for r in range(len(self.board)):
+            #Check if the move is inside the board columns
             for c in range(len(self.board[r])):
+                #Switching between player and zombie turns
                 turn = self.board[r][c]
-                if(turn =="human" and self.humanToMove) or (turn == "zombie" and not self.humanToMove):
+                
+                if(turn == "human" and self.humanToMove) or (turn == "zombie" and not self.humanToMove):
                     player = self.board[r][c][0]
                     if player == "h":
                         self.playerMoves(r, c, moves)
